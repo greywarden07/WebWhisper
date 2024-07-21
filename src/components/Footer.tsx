bd43215b71a0c540f4ec23bf4a4a07564e2a0ba8
greywarden07 <sadhanagautam200@gmail.com>
2024-07-22 00:06:18 +0530

    first commit

---
+++ src/components/Footer.tsx
@@ -0,0 +1,18 @@
+"use client";
+
+import React from "react";
+import Link from "next/link";
+
+const Footer = () => {
+    return (
+        <>
+            <div className="flex flex-row gap-x-10 justify-center">
+                <p className="text-gray text-lg py-8">Copyright 2024 developed by Greywarden. All rights reserved.</p>
+                
+            </div>
+           
+        </>
+    );
+};
+
+export default Footer;
